36598b6233938e8280d9e32bb9e934748f103084
Zak Graber <zakgraber@gmail.com>
2021-02-20 15:33:10 -0800

    Testing logs

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {console.log('Server started at port 8080')});
