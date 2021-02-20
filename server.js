9986447d04011bc4a496b207e7521cf998371078
Zak Graber <zakgraber@gmail.com>
2021-02-20 15:22:00 -0800

    Server adjustments

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(process.env.PORT || 8080);
